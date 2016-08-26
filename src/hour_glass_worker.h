223ab861e76e5584cade59e0fabcbebfa71ed5de
Felix Morgner <felix.morgner@gmail.com>
2016-08-26 12:28:04 +0200

    Remove unnecessary visibility specifiers
    Those were probably left-over from the gool ol' days when you
    implemented C++ via C as a backend for C++ :).

--- src/hour_glass_worker.h
+++ src/hour_glass_worker.h
@@ -8,23 +8,15 @@
 #ifndef HOUR_GLASS_WORKER_H_
 #define HOUR_GLASS_WORKER_H_
 
-#define EXPORT_TO_SO __attribute__ ((visibility ("default")))
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct IntSorter * sorter_t;
 
-EXPORT_TO_SO
 sorter_t sorter_new();
-EXPORT_TO_SO
 void sorter_cleanup(sorter_t sorter);
-
-
-EXPORT_TO_SO
 void sorter_put(sorter_t sorter, int e);
-EXPORT_TO_SO
 int sorter_get(sorter_t sorter);
 
 #ifdef __cplusplus
